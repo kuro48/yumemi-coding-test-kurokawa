1b02ead8764b44fc58f052c94e2ffad0a0c1a4ae
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-11-17 02:04:25 +0900

    update:チェックボックスのUI調整

--- src/components/check-box/check-box.tsx
+++ src/components/check-box/check-box.tsx
@@ -4,9 +4,9 @@ export type Props = {
 
 export default function CheckBox(props: Props) {
   return (
-    <div className="px-3 py-2 flex align-center rounded-md bg-gray-300 justify-center">
-      <input type="checkbox" />
+    <label className="p-2 flex align-center rounded-md bg-gray-300 justify-center">
+      <input type="checkbox" className="w-4" />
       <h2 className="mx-2 text-xl lg:text-base">{props.prefName}</h2>
-    </div>
+    </label>
   );
 }
