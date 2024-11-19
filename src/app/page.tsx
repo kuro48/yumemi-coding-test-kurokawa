5374f46c9ec6b22b969d752a17df0932c5263e17
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-11-20 02:47:37 +0900

    update:チェックボックスからの値の受け渡し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+'use client';
+
 import Prefecture from '@/features/prefecture/components/prefecture';
 import Population from '@/features/population-graph/components/population';
 import { useState } from 'react';
 
 export default function App() {
   const [dataNumber, setDataNumber] = useState<number>(0);
-  const [prefectures, setPrefectures] = useState([]);
-  
+  const [prefectures, setPrefectures] = useState<{ prefCode: number; prefName: string }[]>([]);
+
+  const handlePrefectureChange = (prefCode: number, prefName: string, isChecked: boolean) => {
+    setPrefectures((prev) =>
+      isChecked ? [...prev, { prefCode, prefName }] : prev.filter((p) => p.prefCode !== prefCode)
+    );
+  };
+
   return (
     <>
-      <Prefecture />
+      <Prefecture handleValueChange={handlePrefectureChange} />
       <Population prefectures={prefectures} dataNumber={dataNumber} />
     </>
   );
--- src/components/check-box/check-box.tsx
+++ src/components/check-box/check-box.tsx
@@ -1,11 +1,18 @@
 export type Props = {
+  prefCode: number;
   prefName: string;
+  handleValueChange: (prefCode: number, prefName: string, isChecked: boolean) => void;
 };
 
 export default function CheckBox(props: Props) {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.checked;
+    props.handleValueChange(props.prefCode, props.prefName, value);
+  };
+
   return (
     <label className="p-2 flex align-center rounded-md bg-gray-300 justify-center">
-      <input type="checkbox" className="w-4" />
+      <input type="checkbox" className="w-4" onChange={handleInputChange} />
       <h2 className="mx-2 text-xl lg:text-base">{props.prefName}</h2>
     </label>
   );
