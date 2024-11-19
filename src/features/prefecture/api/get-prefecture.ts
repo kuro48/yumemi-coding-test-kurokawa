066ef00228ae506c8a09132e2e75f10341b16fa9
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-11-20 02:10:52 +0900

    update:都道府県取得機能の修正

--- src/features/prefecture/api/get-prefecture.ts
+++ src/features/prefecture/api/get-prefecture.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
-import { useCallback } from "react";
-import useSWR from "swr";
-import { z } from "zod";
+import axios from 'axios';
+import useSWR from 'swr';
+import { z } from 'zod';
 
 export const getPrefectureResponseSchema = z.object({
   message: z.string().nullable(),
@@ -16,16 +15,15 @@ export const getPrefectureResponseSchema = z.object({
 export type GetPrefectureResponse = z.infer<typeof getPrefectureResponseSchema>;
 
 export default function useGetPrefectureRequest() {
-  const baseURL = process.env.NEXT_PUBLIC_API_URL || "";
-  const requestURL = baseURL + "/api/v1/prefectures";
+  const baseURL = process.env.NEXT_PUBLIC_API_URL || '';
+  const requestURL = baseURL + '/api/v1/prefectures';
 
-  const fetcher = useCallback(() => {
-    console.log("url:", requestURL);
+  const fetcher = () => {
     return axios
       .get(requestURL, {
         headers: {
-          "Content-Type": "application/json; charset=UTF-8",
-          "X-API-KEY": "8FzX5qLmN3wRtKjH7vCyP9bGdEaU4sYpT6cMfZnJ",
+          'Content-Type': 'application/json; charset=UTF-8',
+          'X-API-KEY': '8FzX5qLmN3wRtKjH7vCyP9bGdEaU4sYpT6cMfZnJ',
         },
       })
       .then(async (response) => {
@@ -35,12 +33,9 @@ export default function useGetPrefectureRequest() {
       .catch((error) => {
         throw error;
       });
-  }, [requestURL]);
+  };
 
-  const { data, error, isLoading } = useSWR<GetPrefectureResponse>(
-    requestURL,
-    fetcher
-  );
+  const { data, error, isLoading } = useSWR<GetPrefectureResponse>(requestURL, fetcher);
 
   return { data, error, isLoading };
 }
